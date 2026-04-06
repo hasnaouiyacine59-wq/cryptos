8303e272d486a797fa0b9782394396fe04a079de
oo33 <hasnaouiyacine59@gmail.com>
2026-04-06 16:33:38 +0100

    feat: reload ad iframe on route change

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, useLocation } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Home from './pages/Home'
 import PairPage from './pages/PairPage'
@@ -6,10 +6,12 @@ import Gainers, { Losers } from './pages/Gainers'
 import Watchlist from './pages/Watchlist'
 
 export default function App() {
+  const { pathname } = useLocation()
   return (
     <div className="min-h-screen bg-bg text-white">
       <div className="w-full flex justify-center py-2 bg-bg border-b border-border">
         <iframe
+          key={pathname}
           data-aa='2433217'
           src='//ad.a-ads.com/2433217/?size=728x90'
           style={{ border: 0, padding: 0, width: 728, height: 90, overflow: 'hidden', display: 'block' }}
